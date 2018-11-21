303e7304c1a910b51edb384a0b72b1ba7d952c4a
unknown <itsmetapashya@gmail.com>
2018-11-21 15:39:26 +0530

    Composer Updated

--- web/index.php
+++ web/index.php
@@ -1,3 +1,16 @@
 <?php
-echo "welcome";
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
+
 ?>
